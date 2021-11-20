6b08b74294e9257123a49b6dc616719bd0f73e08
Azain Ayub <azain.ayub2014@gmail.com>
2021-11-20 22:33:50 +0500

    Added the register and login feature

--- assistor/models.py
+++ assistor/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
