d1230d81cabbc3e2d518a664104d0ec7d72b473c
Azain Ayub <azain.ayub2014@gmail.com>
2021-11-21 17:10:07 +0500

    Added course and note model

--- assistor/admin.py
+++ assistor/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Course, Note
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Note)
--- assistor/models.py
+++ assistor/models.py
@@ -6,3 +6,28 @@ from django.db.models.fields.related import ForeignKey
 class User(AbstractUser):
     pass
 
+class Course(models.Model):
+    # User who created the course
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=True)
+
+    # Title of the course
+    title = models.CharField(max_length=60 , null=False, blank=False)
+
+    # Time of creation
+    time = models.DateTimeField(auto_now=True, null=False, blank=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.user}"
+
+class Note(models.Model):
+    # The course note belongs to
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=False, blank=True, related_name="notes")
+
+    # Title of the note
+    title = models.CharField(max_length=60, null=False, blank=False)
+
+    # Content of the note
+    content = models.CharField(max_length=2048 , null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.title}"
