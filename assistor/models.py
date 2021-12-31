61a87badd95a901ac347977be747ebb19ecadcd7
Azain Ayub <azain.ayub2014@gmail.com>
2021-12-31 17:55:15 +0500

    Updated models

--- assistor/models.py
+++ assistor/models.py
@@ -8,13 +8,13 @@ class User(AbstractUser):
 
 class Course(models.Model):
     # User who created the course
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=True, related_name="courses")
 
     # Title of the course
-    title = models.CharField(max_length=60 , null=False, blank=False)
+    title = models.CharField(max_length=64 , null=False, blank=False)
 
     # Time of creation
-    time = models.DateTimeField(auto_now=True, null=False, blank=True)
+    creation_time = models.DateTimeField(auto_now=True, null=False, blank=True)
 
     def __str__(self):
         return f"{self.title} by {self.user}"
@@ -24,10 +24,40 @@ class Note(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=False, blank=True, related_name="notes")
 
     # Title of the note
-    title = models.CharField(max_length=60, null=False, blank=False)
+    title = models.CharField(max_length=64, null=False, blank=False)
 
     # Content of the note
     content = models.CharField(max_length=2048 , null=False, blank=False)
 
+    # Time of creation
+    creation_time = models.DateTimeField(auto_now=True, null=False, blank=True)
+
     def __str__(self):
         return f"{self.title}"
+
+class File(models.Model):
+    # The course where file belongs
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=False, blank=True, related_name="files")
+
+    # Name of the file
+    name = models.CharField(max_length=255, null=False, blank=False)
+
+    # File
+    file = models.FileField(null=False, blank=False)
+
+    # Time of creation
+    creation_time = models.DateTimeField(auto_now=True, null=False, blank=True)
+
+class Reminder(models.Model):
+    # The user who added reminder
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=True, related_name="reminders")
+
+    # Name of the reminder
+    name = models.CharField(max_length=64, null=False, blank=False)
+
+    # Time of the reminder
+    time = models.DateTimeField(null=False, blank=False)
+
+    # Time of creation
+    creation_time = models.DateTimeField(auto_now=True, null=False, blank=True)
+
