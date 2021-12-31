35ec173b7d2ea02f3b93f634e4dd7b2b8ae8d68e
Azain Ayub <azain.ayub2014@gmail.com>
2021-12-31 21:54:09 +0500

    Registered new models

--- assistor/admin.py
+++ assistor/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, Course, Note
+from .models import File, Reminder, User, Course, Note
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Course)
 admin.site.register(Note)
+admin.site.register(Reminder)
+admin.site.register(File)
