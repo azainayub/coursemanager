9aedc767d31aeb3e8cc3e21399438152811ead83
Azain Ayub <azain.ayub2014@gmail.com>
2022-03-02 18:18:08 +0500

    registered instructor and link model?

--- assistor/admin.py
+++ assistor/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import File, Reminder, User, Course, Note
+from .models import File, Reminder, User, Course, Note, Instructor, Link
 
 # Register your models here.
 admin.site.register(User)
@@ -7,3 +7,5 @@ admin.site.register(Course)
 admin.site.register(Note)
 admin.site.register(Reminder)
 admin.site.register(File)
+admin.site.register(Instructor)
+admin.site.register(Link)
