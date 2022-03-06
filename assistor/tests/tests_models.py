2aaaf4dc6848dfbe203a3b3988f8d8f642455660
Azain Ayub <azain.ayub2014@gmail.com>
2022-03-06 22:38:23 +0500

    created tests for Note model

--- assistor/tests/tests_models.py
+++ assistor/tests/tests_models.py
@@ -1,8 +1,8 @@
 from datetime import date, datetime
-from django.test import TestCase, Client
+from turtle import title
+from django.test import TestCase
 
-from assistor.views import index
-from assistor.models import Course, User
+from assistor.models import Course, User, Note, File, Link, Instructor, Reminder
 
 class CourseTestCase(TestCase):
     """Test the course model"""
@@ -25,6 +25,31 @@ class CourseTestCase(TestCase):
         )
         course.save()
         self.assertEqual(Course.objects.count(), 1)
+
+class NoteTestCase(TestCase):
+    """Test the note model"""
+
+    def setUp(self):
+        user = User.objects.create_user(first_name="admin", last_name="admin", username = "admin",
+        email="admin@...", password="admin")
+        user.save()
+        course = Course.objects.create(
+            user=user, 
+            title="Information Security", 
+            start_date=date(2022, 2, 28),
+            completion_date=date(2022, 7, 3),
+            grade=["A"],
+            provider="X University"
+        )
+        course.save()
+
+    def test_note_is_created(self):
+        """Check a note is created"""
+        user = User.objects.get(username="admin")
+        course = Course.objects.get(title="Information Security")
+        note = Note.objects.create(course=course, title="Lorem Ipsum", content="Lorem Ipsum sit amet...........")
+        note.save()
+        self.assertEqual(Note.objects.count(), 1)
 
 class FileModelTestCase(TestCase):
     pass
