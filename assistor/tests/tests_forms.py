b4f581816d174a825234f211bd7552c779ca6fb3
Azain Ayub <azain.ayub2014@gmail.com>
2022-03-06 22:57:07 +0500

    created tests for note form

--- assistor/tests/tests_forms.py
+++ assistor/tests/tests_forms.py
@@ -1,20 +1,14 @@
 from datetime import date, datetime
 from django.test import TestCase, client
 
-from assistor.models import User, Course
-from assistor.forms import CourseForm, LinkForm, InstructorForm, ReminderForm
+from assistor.models import User, Course, Note
+from assistor.forms import CourseForm, NoteForm, FileForm, LinkForm, InstructorForm, ReminderForm, RegistrationForm, LoginForm
 
 class CourseFormTestCase(TestCase):
     """Test the course form"""
 
-    def setUp(self):
-        user = User.objects.create_user(first_name="admin", last_name="admin", username = "admin",
-        email="admin@...", password="admin")
-        user.save()
-
     def test_valid_course_data(self):
         """Check the form with valid data"""
-        user = User.objects.get(username="admin")
         form_data = {
             "title": "Information Security", 
             "start_date": date(2022, 2, 28),
@@ -27,7 +21,6 @@ class CourseFormTestCase(TestCase):
     
     def test_invalid_course_data(self):
         """Check the form with invalid data"""
-        user = User.objects.get(username="admin")
         # Missing title
         form_data = {
             "start_date": date(2022, 2, 28),
@@ -36,4 +29,37 @@ class CourseFormTestCase(TestCase):
             "provider": "X University"
         }
         form = CourseForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+class NoteFormTestCase(TestCase):
+    """Test the note form"""
+
+    def setUp(self):
+        user = User.objects.create_user(first_name="admin", last_name="admin", username = "admin",
+        email="admin@...", password="admin")
+        user.save()
+        course = Course.objects.create(user=user, title="Information Security")
+        course.save()
+
+    def test_valid_course_data(self):
+        """Check the form with valid data"""
+        form_data = {
+            "title": "Lorem Ipsum",
+            "content": "Lorem Ipsum sit amet ......"
+        }
+        form = NoteForm(data=form_data)
+        self.assertTrue(form.is_valid())
+    
+    def test_invalid_course_data(self):
+        """Check the form with invalid data"""
+        # Missing title and title exceeding max length
+        form_data = {
+            "title": """Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+            Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+            Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+            Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+            Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+            Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum""",
+        }
+        form = NoteForm(data=form_data)
         self.assertFalse(form.is_valid())
